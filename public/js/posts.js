2b7d2d8b69240472acae6bee24dbeb185ffc2061
Tasneem <thalim.glam@gmail.com>
2024-01-18 13:20:34 -0800

    Changed several files to successfully log in and create post

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,38 +1,30 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
+// const newFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const name = document.querySelector('#post-name').value.trim();
-  const description = document.querySelector('#post-desc').value.trim();
-
-
-  if (response.ok) {
-    document.location.replace('/posts');
-  } else {
-    alert('Failed to create post');
-  }
-};
+//   const name = document.querySelector('#post-name').value.trim();
+//   const description = document.querySelector('#post-desc').value.trim();
 
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE',
-    });
+//     const response = await fetch(`/api/posts/${id}`, {
+//       method: 'DELETE',
+//     });
 
-    if (response.ok) {
-      document.location.replace('/posts');
-    } else {
-      alert('Failed to delete post');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/posts');
+//     } else {
+//       alert('Failed to delete post');
+//     }
+//   }
+// };
 
-document
-  .querySelector('.new-post-form')
-  .addEventListener('submit', newFormHandler);
+// document
+//   .querySelector('.new-post-form')
+//   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.post-list')
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('.post-list')
+//   .addEventListener('click', delButtonHandler);
