ca4d5c6405e7478fac81455f140fb71bc861da29
Tasneem <thalim.glam@gmail.com>
2024-01-18 10:51:59 -0800

    Working on blogPost

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,14 +2,41 @@ const router = require('express').Router();
 
 const { Blog } = require('../../models');
 
-router.post('/', async (req,res) => {
-  const body = req.body
-
+router.post('/', async (req, res) => {
   try {
-    const newBlogPost = await Blog.create({...body, userId: req.session.userId})
-    res.json(newBlogPost)
-  } catch (error) {
-    res.status(500).json(error)
+    const { title, body } = req.body;
+    const newPost = await Post.create({
+      name: title,
+      description: body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
   }
-})
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
